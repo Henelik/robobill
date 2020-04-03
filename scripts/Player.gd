543d2304855aacf8fdae62ea21c4aa966fb0a230
Kelyn Crandall <kelyn@crandall.me>
2020-04-02 21:47:53 -0500

    Added leg animations

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
+export (int) var max_speed = 200
 export (NodePath) var body_path
 export (NodePath) var light_path
 var body
@@ -12,25 +12,45 @@ var _light_toggle_time = 0
 var velocity = Vector2()
 var mouse_pos
 
+export (NodePath) var legs_path
+export (NodePath) var legs_player_path
+var legs_rotation_speed = .5
+var legs
+var legs_player
+
+var walking
+
 func _ready():
 	body = get_node(body_path)
 	light = get_node(light_path)
-	
+	legs = get_node(legs_path)
+	legs_player = get_node(legs_player_path)
 
 func get_input():
 	velocity = Vector2()
 	_light_toggle_time -= 1
+	walking = false
 	if Input.is_action_pressed('right'):
 		velocity.x += 1
+		walking = true
 	if Input.is_action_pressed('left'):
 		velocity.x -= 1
+		walking = true
 	if Input.is_action_pressed('down'):
 		velocity.y += 1
+		walking = true
 	if Input.is_action_pressed('up'):
 		velocity.y -= 1
+		walking = true
 	if Input.is_action_pressed('light'):
 		toggle_light()
-	velocity = velocity.normalized() * speed
+	if velocity.x != 0 || velocity.y != 0:
+		legs_player.play("Walking")
+	else:
+		legs_player.play("Idle")
+	if walking:
+		velocity = velocity.normalized() * max_speed
+		legs.rotation = velocity.angle()*legs_rotation_speed + legs.rotation*(1-legs_rotation_speed)
 	
 func toggle_light():
 	if _light_toggle_time <= 0:
