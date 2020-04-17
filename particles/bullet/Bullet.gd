6c9b844e7521c738a069115831f71c74c89edddf
Kelyn Crandall <kelyn@crandall.me>
2020-04-16 23:32:18 -0500

    Added damage logging

--- particles/bullet/Bullet.gd
+++ particles/bullet/Bullet.gd
@@ -2,8 +2,13 @@ extends RigidBody2D
 
 export (float) var initial_force = 1000
 
+var damage : int = 5
+
 func projectile_ready():
 	apply_central_impulse(global_transform.x*initial_force)
 	
 func _on_Bullet_body_entered(body):
+	if body.is_in_group("Enemy"):
+		print("Dealt " + String(damage) + " damage")
+		body.damage(damage, "pierce")
 	queue_free()
