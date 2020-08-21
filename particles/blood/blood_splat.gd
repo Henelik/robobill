80e32abe15f24d130a3355c199986bb2355fc8eb
Kelyn Crandall <kelyn@crandall.me>
2020-08-21 14:33:48 -0500

    Added blood effects

--- particles/blood/blood_splat.gd
+++ particles/blood/blood_splat.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
+export(Array, Texture) var textures
 
 func _ready():
 	$Sprite/AnimationPlayer.play("splat")
-
+	scale.y = pow(-1, randi() % 2) # random 1 or -1 y scale
+	$Sprite.texture = textures[randi() % textures.size()] # random texture from the array
+	$BloodSmoke.emitting = true
+	$Squib.emitting = true
